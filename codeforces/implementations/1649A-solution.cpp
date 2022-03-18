06413a1269bd632fbac7266908c3b4ad3d86bfbb
eliemoutran <54373745+eliemoutran@users.noreply.github.com>
2022-03-18 18:09:00 +0200

    Update 1649A-solution.cpp

--- codeforces/implementations/1649A-solution.cpp
+++ codeforces/implementations/1649A-solution.cpp
@@ -1,4 +1,4 @@
-//athour Elie Moutran
+//author Elie Moutran
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -17,18 +17,20 @@ int main()
 
         int n = locs.size();
         int i,j; 
+        // Looping forward in the vector to find the first occurence of 0
         for(i = 0; i < n; i++)
         {
             if(locs[i] == 0)
                 break;
         }
-        
+        // Looping backwards in the vector to find the last occurence of 0
         for(j = n-1; j >= i ; j--)
         {
             if(locs[j] == 0)
                 break;
         }
         
+        // if i==n that means no zero was found
         if(i != n)
             cout<< j-i+2;
         else
