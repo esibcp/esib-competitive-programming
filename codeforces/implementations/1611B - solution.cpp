1d108d122975deb568d802a425a1ae034c161a20
Emmanuel Abdelnour <94867102+Emmanuelabn@users.noreply.github.com>
2022-03-23 01:55:12 +0200

    Add files via upload

---
+++ codeforces/implementations/1611B - solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int &a, int &b){
+    a = a + b;
+    b = a - b;
+    a = a - b;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t>0){
+        long long a, b, team(0);
+        cin >> a >> b;
+        if(a < b) swap(a, b);
+        if(b * 3 <= a) team = b;
+        else team = (a + b) / 4;
+        cout << team << endl;
+        --t;
+    }
+}
