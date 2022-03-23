52bfabcb82468628b6cd58c74504aee5ab4a9cf2
Maroun Ayli <marounayle@gmail.com>
2022-03-23 22:17:20 +0200

    refactroed

--- codeforces/implementations/1654A  -  solution(2).cpp
+++ codeforces/implementations/1654A  -  solution(2).cpp
@@ -12,12 +12,14 @@ int main(){
         cin >> n;
         vector<int> v(n);
         for (int i = 0; i < n; i++) cin >> v[i];
-        max1 = max(v[0], v[1]);
-        max2 = min(v[0], v[1]);
-        for (int i = 2; i < n; i++)
+        // no need to call max before the loop  and start the loop at index 2.
+        //Guarantee that the max will be changed by declaring it the smallest possible value
+        //Gigachad move (meron)
+        int max1 = INT32_MIN, max2 = INT32_MIN;
+        for (int i = 0; i < n; i++)
         {
             if(v[i] >= max1) {
-                max2 = max1; 
+                max2 = max1;
                 max1 = v[i];
             }
             if(v[i] < max1 && v[i] > max2) max2 = v[i];
